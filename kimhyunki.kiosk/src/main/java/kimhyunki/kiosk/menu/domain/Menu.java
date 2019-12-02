cc316f0ba037ba162062c20f2555d7c613395986
SIST100 <SIST100@DESKTOP-N4V47JI>
2019-12-02 19:33:36 +0900

    2019-12-02 김현기 login,menu,order 도메인생성, xml설정파일 설정

--- kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/menu/domain/Menu.java
+++ kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/menu/domain/Menu.java
@@ -1,5 +1,52 @@
 package kimhyunki.kiosk.menu.domain;
 
 public class Menu {
-
+	private int MenuId;
+	private String menuName;
+	private String menuImg;
+	private String category;
+	private int menuPrice;
+	private String recommend;
+	public int getMenuId() {
+		return MenuId;
+	}
+	public void setMenuId(int menuId) {
+		MenuId = menuId;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public String getMenuImg() {
+		return menuImg;
+	}
+	public void setMenuImg(String menuImg) {
+		this.menuImg = menuImg;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getMenuPrice() {
+		return menuPrice;
+	}
+	public void setMenuPrice(int menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+	public String getRecommend() {
+		return recommend;
+	}
+	public void setRecommend(String recommend) {
+		this.recommend = recommend;
+	}
+	@Override
+	public String toString() {
+		return String.format("%s %s %s %s %s %s", MenuId, menuName, menuImg, category, menuPrice, recommend);
+	}
+	
+	
 }
--- kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/order/domain/Order.java
+++ kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/order/domain/Order.java
@@ -1,5 +1,31 @@
 package kimhyunki.kiosk.order.domain;
 
 public class Order {
-
+	private int orderNo;
+	private String place;
+	private String orderStatus;
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	@Override
+	public String toString() {
+		return String.format("%s %s %s", orderNo, place, orderStatus);
+	}
+	
+	
 }
