8ffb63880e2659e475d8ef41656b87a8c4a486c6
youngjo4study <50536956+youngjo4study@users.noreply.github.com>
2019-12-16 23:07:26 +0900

    Add files via upload

--- kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/login/domain/Login.java
+++ kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/login/domain/Login.java
@@ -1,7 +1,16 @@
 package kimhyunki.kiosk.login.domain;
 
 public class Login {
-	private int password;
+	private int password;	
+	private String logoImg;
+	
+	public String getLogoImg() {
+		return logoImg;
+	}
+
+	public void setLogoImg(String logoImg) {
+		this.logoImg = logoImg;
+	}
 
 	public int getPassword() {
 		return password;
