4439c8f3036056eef594977972c344e12631944e
youngjo4study <50536956+youngjo4study@users.noreply.github.com>
2019-12-16 23:05:56 +0900

    loginController에 로고이미지 업로드 추가함

--- kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/login/controller/LoginController.java
+++ kimhyunki.kiosk/src/main/java/kimhyunki/kiosk/login/controller/LoginController.java
@@ -1,19 +1,28 @@
 package kimhyunki.kiosk.login.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import kimhyunki.kiosk.login.domain.Login;
 import kimhyunki.kiosk.login.service.LoginService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 	@Autowired private LoginService loginService;
+	
+	@Value("${uploadDir}")
+    private String uploadDir;
 	
 	@RequestMapping("login")
 	public void login(){}
@@ -22,6 +31,41 @@ public class LoginController {
 	public void adminPage(){
 
 	}
+	
+	
+	@RequestMapping("imgManage")
+	public void imgManage(){
+		
+	}
+	
+	@RequestMapping("addLogo")
+	@ResponseBody
+	public String addLogo(String logoImg){	
+		boolean flag = loginService.addLogo(logoImg);
+		return "login/adminMenu";
+	}
+	
+	@RequestMapping("insertLogo")
+    @ResponseBody
+    public boolean upload(MultipartFile uploadFile, HttpServletRequest request){
+        boolean isStored = true;
+        String dir = "C:/DEV/git/kimhyunki.kiosk/kimhyunki.kiosk/src/main/webapp/WEB-INF/res/img";
+	    System.out.println("dir: "+dir);
+	    String fileName = uploadFile.getOriginalFilename(); 
+	    System.out.println(fileName);
+	    try{
+	        save(dir +"/"+ fileName, uploadFile);
+	    }catch(IOException e){
+	            isStored = false;
+	    }
+	    return isStored;
+    }
+	
+	private void save(String fileFullName, MultipartFile uploadFile) throws IOException{
+        uploadFile.transferTo(new File(fileFullName));
+    }
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	@RequestMapping("/check")
 	@ResponseBody
